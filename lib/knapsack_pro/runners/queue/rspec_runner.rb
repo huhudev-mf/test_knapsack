d589ead9c1a46aac2630dc64002c66a8522c5e86
huhu <contact@huhu.dev>
2021-03-25 16:44:44 +0900

    update

--- lib/knapsack_pro/runners/queue/rspec_runner.rb
+++ lib/knapsack_pro/runners/queue/rspec_runner.rb
@@ -42,7 +42,7 @@ module KnapsackPro
           else
             options = ::RSpec::Core::ConfigurationOptions.new(cli_args + [test_file_path])
             exit_code = ::RSpec::Core::Runner.new(options).run($stderr, $stdout)
-            RSpec.clear_examples
+            rspec_clear_examples
             exitstatus = exit_code if exit_code != 0
           end
           
@@ -50,6 +50,28 @@ module KnapsackPro
             status: :next,
             exitstatus: exitstatus,
           }
+        end
+
+        def self.rspec_clear_examples
+          if ::RSpec::ExampleGroups.respond_to?(:remove_all_constants)
+            ::RSpec::ExampleGroups.remove_all_constants
+          else
+            ::RSpec::ExampleGroups.constants.each do |constant|
+              ::RSpec::ExampleGroups.__send__(:remove_const, constant)
+            end
+          end
+          ::RSpec.world.example_groups.clear
+          ::RSpec.configuration.start_time = ::RSpec::Core::Time.now
+
+          # Reset example group counts to ensure scoped example ids in metadata
+          # have correct index (not increased by each subsequent run).
+          # Solves this problem: https://github.com/rspec/rspec-core/issues/2721
+          ::RSpec.world.instance_variable_set(:@example_group_counts_by_spec_file, Hash.new(0))
+
+          # skip reset filters for old RSpec versions
+          if ::RSpec.configuration.respond_to?(:reset_filters)
+            ::RSpec.configuration.reset_filters
+          end
         end
       end
     end
